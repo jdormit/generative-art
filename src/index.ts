40142d6afdace522398f6df82af7b358be1d221e
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-08-14 15:12:25 -0400

    Tests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import * as seedrandom from "seedrandom";
+import * as seedrandom from 'seedrandom';
+import * as guassian from 'gaussian';
 
 const canvas = <HTMLCanvasElement> document.getElementById('canvas')!
 const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
+
+ctx.fillStyle = 'rgb(200,0,0)';
+ctx.fillRect(10, 10, 50, 50);
