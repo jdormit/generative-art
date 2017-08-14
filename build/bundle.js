00155c173b3ee9125378556bd3770ff4bf8737e0
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-08-14 15:12:37 -0400

    Build and package lock

--- build/bundle.js
+++ build/bundle.js
@@ -72,6 +72,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'rgb(200,0,0)';
+ctx.fillRect(10, 10, 50, 50);
 
 
 /***/ })
